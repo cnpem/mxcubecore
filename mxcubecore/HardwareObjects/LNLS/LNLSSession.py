165131baf84da5dc2ccc8bca470dfc1b32dfca84
Pedro Benetton <pedro.benetton@lnls.br>
2026-05-28 12:32:47 -0300

    Fix proposal code

--- mxcubecore/HardwareObjects/LNLS/LNLSSession.py
+++ mxcubecore/HardwareObjects/LNLS/LNLSSession.py
@@ -7,7 +7,7 @@ from mxcubecore.HardwareObjects.Session import Session
 class LNLSSession(Session):
     def get_proposal(self):
         proposal = super().get_proposal()
-        return proposal.replace("sc", "")
+        return proposal.replace("sc", "").replace("rap", "")
 
     def get_base_image_directory(self):
         start_time = time.strftime("%Y%m%d")
