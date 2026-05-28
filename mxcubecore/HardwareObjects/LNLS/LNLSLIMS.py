42eafedd96767448793cd947ee3ac83783df8782
Pedro Benetton <pedro.benetton@lnls.br>
2026-05-28 12:32:47 -0300

    Remove log prints from login

--- mxcubecore/HardwareObjects/LNLS/LNLSLIMS.py
+++ mxcubecore/HardwareObjects/LNLS/LNLSLIMS.py
@@ -35,13 +35,9 @@ class LNLSLIMS(ICATLIMS):
 
     def login(self, user_name, token, is_local_host):
         self.is_local_host = is_local_host
-        self.log.debug(f"is_local_host: {is_local_host}")
         session_manager, lims_username, sessions = super().login(
             user_name, token, self.session_manager
         )
-        self.log.debug(f"session_manager: {session_manager}")
-        self.log.debug(f"lims_username: {lims_username}")
-        self.log.debug(f"sessions: {sessions}")
         self.session_manager = session_manager
         self.add_user_and_shared_sessions(lims_username, sessions)
         if self.is_single_session_available():
