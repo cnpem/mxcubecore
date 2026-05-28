821dbe70056c856128bad28b101f3053adf06465
Pedro Benetton <pedro.benetton@lnls.br>
2026-05-28 12:32:47 -0300

    Add initial version of LNLSSession
    It builds the proper path for saving files. Also, there is a little
    workaround for the proposal which will be removed later.

---
+++ mxcubecore/HardwareObjects/LNLS/LNLSSession.py
@@ -0,0 +1,16 @@
+from mxcubecore.HardwareObjects.Session import Session
+import time
+import os
+
+class LNLSSession(Session):
+    def get_base_data_directory(self):
+        start_time = time.strftime("%Y%m%d")
+        proposal = self.get_proposal()
+        proposal = proposal.replace("test0020", "00000000")
+        directory = os.path.join(
+                self.base_directory,
+                proposal,
+                "data",
+                start_time,
+            )
+        return directory
